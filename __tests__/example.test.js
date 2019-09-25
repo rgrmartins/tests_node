0b41b22ab2af35701661281c50a05f4fd6d8ad3d
roger <rogermartinsdesign@gmail.com>
2019-09-25 16:54:49 -0400

    Testes no Node com TDD e Jest

---
+++ __tests__/example.test.js
@@ -0,0 +1,11 @@
+// Example
+function soma(a, b) {
+  return a + b;
+}
+
+test('if i call soma function with 4 and 5 it should return 9', () => {
+  const result = soma(4, 5);
+
+  // Criando teste
+  expect(result).toBe(9);
+});
